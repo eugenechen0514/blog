a2be8753056d899d2f74e8cc31b3aa88db69ce25
EugeneChen <yujiechen0514@gmail.com>
2022-06-25 19:21:17 +0800

    init project

---
+++ assets/icons/custom.js
@@ -0,0 +1,12 @@
+import {
+    faBook, faBug, faHeart, faImage, faImages, faNewspaper,
+} from '@fortawesome/free-solid-svg-icons';
+// import { faAddressBook } from '@fortawesome/free-regular-svg-icons';
+// import { faAmazon, faGoogle } from '@fortawesome/free-brands-svg-icons';
+
+const icons = [
+    faBook, faBug, faHeart, faImage, faImages, faNewspaper,
+    // faAddressBook,
+    // faAmazon, faGoogle,
+];
+export default icons;
---
+++ content/about/index.md
@@ -0,0 +1,12 @@
++++
+title = "About"
+description = ""
+date = "2022-06-23"
+aliases = ["about-us"]
+author = "Eugene"
+reward = false
+postDate = false
+readingTime = false
++++
+
+WIP
